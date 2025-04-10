4a7b986d0863ef0a5cb159999080d4aae533b9f0
khaoula hassoune <khaoulahassoune1@gmail.com>
2025-04-10 13:43:00 +0100

    update

--- app.js
+++ app.js
@@ -20,8 +20,11 @@ const app = express();
 
 // Middleware
 app.use(express.json()); // Parse JSON bodies
+
+
 app.use(cors({
     origin: 'http://localhost:5173',
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
